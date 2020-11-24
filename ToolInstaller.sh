2d3a8ec735553c23607d981144e181f8bcc8cfac
Z3RØ_AN7RÄX <70908677+js13-Hacker@users.noreply.github.com>
2020-11-24 20:19:34 -0300

    Add files via upload
    cd ToolInstaller
    bash ToolInstaller.sh

---
+++ ToolInstaller.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+clear
+
+echo  "[•]Created by:Z3RØ_AN7RÄX" | lolcat
+
+figlet -f mono12 "ToolInstaller" | lolcat
+
+echo "[•]Isso pode demorar alguns minutos" | lolcat
+
+pkg upgrade && pkg update 
+
+pkg install curl 
+
+pkg install wget
+
+pkg install lolcat
+
+pkg install python
+
+pkg install python2
+
+pkg install python3
+
+pkg install pip
+
+pkg install pip2
+
+pkg install irssi
+
+pkg install php
+
+pkg install git
+
+pkg install openssh
+
+git clone https://github.com/cyweb/hammer
+
+git clone https://github.com/Gameye98/Lazymux
+
+git clone https://github.com/LimerBoy/Impulse
+
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+
+git clone https://github.com/TheReaper167/Malicious
+
+git clone https://github.com/hahwul/a2sv
+
+git clone https://github.com/sandiwijayani1/SpamCall-1
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+
+git clone https://github.com/hangetzzu/saycheese
+
+git clone https://github.com/maldevel/IPGeoLocation
+
+git clone https://github.com/abhinavkavuri/PhoneInfoga
+
+git clone https://github.com/Hacking-pch/papaviruz
+
+git clone https://github.com/RealHackRH593/CC-MundoRH
+
+git clone https://github.com/T3rMuX0/Consulta-v3
+
+git clone https://github.com/Cesar-Hack-Gray/scam
+
+git clone https://github.com/PSecurity/ps.ngrok
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+git clone https://github.com/TermuxHacking000/PortmapSploit
+
+git clone https://github.com/penetrate2hack/ITWSV
+
+git clone https://github.com/uhusmanhaider/facebook-report
+
+git clone https://github.com/darkcurut08/darkcurut08
+
+git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+
+git clone https://github.com/aboul3la/Sublist3r
+
+git clone https://github.com/rajkumardusad/IP-Tracer
+
+git clone https://github.com/RealHackRH593/BinsGenRH
+
+git clone https://github.com/suyash6/hulk
+
+git clone https://github.com/JoeTech-Studio/Bingen
+
+git clone https://github.com/TermuxHacking000/PortmapSploit.git
+
+git clone https://github.com/penetrate2hack/ITWSV.git
+
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+
+git clone https://github.com/dizart-y/cLONiNg-Yahoo
+
+git clone https://github.com/perjayro/MultiPhish.git
+
+git clone https://github.com/luisakrac/WFIKRACC.git
+
+git clone https://github.com/threat9/routersploit
+
+git clone https://github.com/Bl4ckDr460n/Black-Fb-Premium
+
+git clone https://github.com/Bl4ckDr460n/G-Dork
+
+git clone https://github.com/Bl4ckDr460n/SPAMer
+
+git clone https://github.com/Bl4ckDr460n/My-Ip
+
+git clone https://github.com/Bl4ckDr460n/STrojans
+
+git clone https://github.com/zlucifer/lucita_ddos
+
+git clone https://github.com/MrUncle/PulsaGratis
+
+git clone https://github.com/MrUncle/Script-HackBank
+
+git clone https://github.com/T14RB373T/FAKECALL
+
+echo "[•]PRONTO" | lolcat
